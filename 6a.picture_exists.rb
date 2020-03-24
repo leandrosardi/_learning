edc18f2d1bde84ad962a74a1fe64cf0f10eaa7d5
leandrosardi <leandro.sardi@expandedventure.com>
2020-03-24 10:23:08 -0300

    adding example script to check if a picture exists in the website

--- 6a.picture_exists.rb
+++ 6a.picture_exists.rb
@@ -1,10 +1,22 @@
 require 'net/http'
 require 'uri'
+require 'openssl'
 #require 'websocket'
 #require 'json'
 
+
+print 'Right URL... '
 pic_url = 'https://connectionsphere.com:443/clients/B1D94DDD-D9FA-4BB3-AD6A-9B65AB161295/mac-stock-pictures/andrea.beltran@virgipx.xyz.jpg'
 uri = URI(pic_url)
-request = Net::HTTP.new uri.host
-response= request.request_head uri.path
-raise "Picture file not exists (#{pic_url.to_s} -> #{response.code.to_s})" if response.code.to_i != 200
+request = Net::HTTP.new(uri.host, uri.port)
+request.use_ssl = true
+response = request.request_head(uri.path)
+puts "response: #{response.code.to_s}"
+
+print 'Wrong URL... '
+pic_url = 'https://connectionsphere.com:443/clients/B1D94DDD-D9FA-4BB3-AD6A-9B65AB161295/mac-stock-pictures/andrea.beltran@virgipx.xyz.jpg.not_exists'
+uri = URI(pic_url)
+request = Net::HTTP.new(uri.host, uri.port)
+request.use_ssl = true
+response = request.request_head(uri.path)
+puts "response: #{response.code.to_s}"
